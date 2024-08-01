26197199673beb826584884371c054905d057bee
Kyra4code <lucarioecharmito@gmail.com>
2024-07-31 22:40:51 -0300

    segunda

--- index.js
+++ index.js
@@ -5,12 +5,32 @@ async function pegardados(){
    return dados;
 };
 
-async function exemplo01(){
+/*async function exemplo01(){
     const variavel = await pegardados();
     variavel.forEach(element => {
         console.log(element.gender);
     });
 }
 
+async function exemploMap(){
+    const variavel = await pegardados
 
-exemplo01();
+    const dadosFinais = variavel.map(pessoa =>{
+        return{
+        "nomeCompleto": pessoa.name.first,
+        "SegundoNome": pessoa.name.last,
+        "Email": pessoa.email
+        }
+        console.log(dadosFinais);
+    })
+}*/
+
+async function Atividade(){
+    const x = await pegardados();
+
+    const y = x.map(pessoa =>{
+        return{
+            "NomeCompleto": `${pessoa.name.first}`
+        }
+    })
+}
