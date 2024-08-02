259d738357d2ea2670744bdafa86b5b9aa9d66c3
Kyra4code <lucarioecharmito@gmail.com>
2024-08-02 20:34:18 -0300

    find_filter

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 import {promises as fs} from "fs";
 
-async function pegardados(){
-   const dados = JSON.parse(await fs.readFile("./people.json"));
-   return dados;
-};
+const dados = JSON.parse(await fs.readFile("./people.json"));
 
 /*async function exemplo01(){
-    const variavel = await pegardados();
+    const variavel = await dados;
     variavel.forEach(element => {
         console.log(element.gender);
     });
 }
 
 async function exemploMap(){
-    const variavel = await pegardados
+    const variavel = await dados
 
     const dadosFinais = variavel.map(pessoa =>{
         return{
@@ -25,12 +22,34 @@ async function exemploMap(){
     })
 }*/
 
-async function Atividade(){
-    const x = await pegardados();
+/*async function Atividade(){
+    const x = await dados
 
     const y = x.map(pessoa =>{
         return{
-            "NomeCompleto": `${pessoa.name.first}`
+            "NomeCompleto": `${pessoa.name.first} ${pessoa.name.last}`,
+            "Genero": `${pessoa.gender}`,
+            "Endereço": `${pessoa.location.street.name} ${pessoa.location.street.number}`,
+            "NumeroTelefone": `${pessoa.cell}`
         }
     })
+    console.log(y);
 }
+y()*/
+/*var f = ()=>{
+    const filtro = dados.filter(pessoa =>{
+        return pessoa.gender == "male";
+    })
+    console.log(filtro)
+}
+
+f();*/
+
+var f2 = ()=>{
+    const find = dados.find(pessoa =>{
+        return pessoa.gender == "male"
+    })
+    console.log(find)
+}
+f2()
+
