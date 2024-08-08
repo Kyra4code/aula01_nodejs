cc11ce12edbeb73fc98bd3a8b980254233c2e183
Kyra4code <lucarioecharmito@gmail.com>
2024-08-07 21:41:29 -0300

    som-touppercase

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import {promises as fs} from "fs";
 
 const dados = JSON.parse(await fs.readFile("./people.json"));
 
+//ForEach é como um for com text.lenth
+
 /*async function exemplo01(){
     const variavel = await dados;
     variavel.forEach(element => {
         console.log(element.gender);
     });
 }
+
+//Percorre um arquivo guardando a informação na variável
 
 async function exemploMap(){
     const variavel = await dados
@@ -36,6 +40,9 @@ async function exemploMap(){
     console.log(y);
 }
 y()*/
+
+//filter filtra itens que no codigo é igual, exemplo gender male só vai aparecr pessoas com sexo male
+
 /*var f = ()=>{
     const filtro = dados.filter(pessoa =>{
         return pessoa.gender == "male";
@@ -45,11 +52,22 @@ y()*/
 
 f();*/
 
-var f2 = ()=>{
+//find é basicaente um filter so que ele só puxa 1 pessoa
+
+/*var f2 = ()=>{
     const find = dados.find(pessoa =>{
         return pessoa.gender == "male"
     })
     console.log(find)
 }
-f2()
+f2()*/
+
+var f3 = ()=>{
+    const existeMasculino = dados.some(pessoa =>{
+        return pessoa.location.state.toUpperCase() == "xique-xique".toUpperCase();
+    })
+    console.log(existeMasculino)
+}
+
+f3();
 
